92926e7aea964e2d143c5d1b64df28a6fe8ac82e
arthur-manske <arthur-manske@proton.me>
2025-11-29 06:49:10 -0300

    contemplem o mago, com seus poderes
    incriveis poderes!!

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,24 +1,22 @@
-    import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 
-    import { JWT_SECRET, JWT_EXPIRES } from "../config/jwt";
+import { JWT_SECRET, JWT_EXPIRES } from "../config/jwt";
 
-    type AuthToken = string | any;
+type AuthToken = string | any;
 
-    export class AuthService {
-        public static tokenFrom(payload: string | object): AuthToken
-        {
-            return jwt.sign(payload, JWT_SECRET, {
-                algorithm: "HS256",
-                expiresIn: JWT_EXPIRES,
-            });
-        }
+export class AuthService {
+    public static tokenFrom(payload: string | Buffer | object): AuthToken {
+        return jwt.sign(payload as string, JWT_SECRET as string, {
+            algorithm: "HS256",
+            expiresIn: JWT_EXPIRES,
+        } as SignOptions);
+    }
 
-        public static fromToken(token: AuthToken): any | Object | null
-        {
-            try {
-                return jwt.verify(token as string, JWT_SECRET) as any;
-            } catch {
-                return null;
-            }
+    public static fromToken(token: AuthToken): any | Object | null {
+        try {
+            return jwt.verify(token as string, JWT_SECRET) as any;
+        } catch {
+            return null;
         }
     }
+}
