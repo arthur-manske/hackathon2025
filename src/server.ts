49cf258ef0475453398a967b1c64faf39a0d5b78
arthur-manske <arthur-manske@proton.me>
2025-11-29 07:36:03 -0300

    Smells like cat spirit

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import rateLimit                from 'express-rate-limit';
 import { AppDataSource } from "./config/data-source";
 
 import PatientRoutes from "./routes/PatientRoutes";
+import UserRoutes from "./routes/UserRoutes";
 
 const app:  Application = express();
 const port: number      = Number(process.env.PORT) || 3000;
@@ -35,6 +36,7 @@ app.use(rateLimit({
 }));
 
 app.use("/patients", PatientRoutes);
+app.use("/users", UserRoutes);
 
 app.get("/", (_req, res) => {
     res.status(201).json({message: "Server is up."});
