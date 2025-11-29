8824fbf063a022b2e0d1ad87b9a33cddf7481a51
ARTHUR DE SOUZA MANSKE <05701722090@senacrs.edu.br>
2025-11-28 22:56:29 -0300

    data-source: entities fix

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,6 +3,8 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 
+import { Patient } from "../entities/Patient";
+
 dotenv.config()
 
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
@@ -16,5 +18,5 @@ export const AppDataSource = new DataSource({
     database: DB_NAME,
     synchronize: true,
     logging: true,
-    entities: ["src/entities/*.ts"]
+    entities: [Patient]
 });
